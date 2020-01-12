d6ed4c2c95b0f4d4e192bc79b720d7699b5e9645
Mike Mercuro <mmercuro21@gmail.com>
2020-01-12 13:15:36 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data () {
+    return {
+      title: 'My portfolio'
+    }
+  },
+  mounted () {
+    const $ = this.jquery
+    this.$nextTick(() => {
+      // our custom jQuery code goes here
+    })
+  }
+}
+</script>
